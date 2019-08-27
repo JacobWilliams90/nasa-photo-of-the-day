08ed589ce5f615363c9e4e1e3e5514c5b68560e4
Jacob Williams <JaWilliamsDev@gmail.com>
2019-08-27 11:39:53 -0400

    Initial Push, + started working on creating the site

---
+++ src/head.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div className='header'>
+            {
+            <h1>Nasa Photo of the day</h1>
+            }
+            {
+            <a href='http://www.nasa.gov'>Take me back to NASA</a>
+            }
+        </div>
+    )
+}
