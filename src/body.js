58d5b20120063fc4d82849ff807dd38a5e2b3930
Jacob Williams <JaWilliamsDev@gmail.com>
2019-08-29 16:13:06 -0400

    Removed file for the axios pull, moved into body.js for visability reasons

--- src/body.js
+++ src/body.js
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { getNasaImg } from './nasapic'
+import axios from 'axios';
+// import { getNasaImg } from './nasapic'
 import { StyledImg } from './StyledImg'
 import { StyledBody } from './StyledBody'
 
-const Body = () => {
+const Body = (prop) => {
+    const [title, setTitle] = useState();
     const [img, setImg] = useState();
     const [date, setDate] = useState();
     const [des, setDes] = useState();
 
-    useEffect(() =>{
-        getNasaImg(setImg, setDate, setDes);
-    },[]);
+    // useEffect(() =>{
+    //     getNasaImg(setTitle,setImg, setDate, setDes);
+    // },[]);
+
+    useEffect(() => {
+        function getNasaImg(title,pic,date,des){
+            axios
+                .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+                .then(res => {
+                    title(res.data.title);
+                    pic(res.data.hdurl); 
+                    date(res.data.date);
+                    des(res.data.explanation);
+                })
+                .catch(err => console.log(err))
+        }
+        getNasaImg(setTitle, setImg, setDate, setDes)
+    },[])
 
     if (!img) return <h3>Loading...</h3>;
 
     return (
         <StyledBody>
-            <h2>{date}</h2>
+            <h2>{title}</h2>
+            <h4>{date}</h4>
             <p>{des}</p>
             <StyledImg src={img} />
         </StyledBody>
